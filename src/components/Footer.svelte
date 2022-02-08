57f4ba6c7b8bb56fb7b9f72a965995888251b4c9
Georgy <georgy.1010.011@gmail.com>
2022-02-07 18:07:44 -0600

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  let actualYear = new Date().getFullYear();
+</script>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      © {actualYear} FISHSTAGRAM FROM DAVID OJEDA
+    </div>
+  </div>
+</div>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
